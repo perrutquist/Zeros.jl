ffba5d627947fbd1b7fb930d4cac91580d0c4c03
Per Rutquist <per.rutquist@gmail.com>
2018-10-20 21:04:24 +0200

    Re-structuring

--- src/codegen.jl
+++ src/codegen.jl
@@ -9,44 +9,64 @@ module TemporaryCodegenModule
 
 using StaticNumbers
 
+include("methods.jl")
+
 d = Dict((-1 => :MinusOne, 0 => :Zero, 1 => :One))
 
 f1 = (+, -, real, imag, sin, cos, tan, sinpi, cospi, sign,)
 
 f2 = (+, -, *, /)
 
-println("# Functions with one argument")
-for f in f1, i in keys(d)
-    try
-        r = @eval $f($i)
+function fun1(f1, d)
+    println("# Functions with one argument")
+    for f in f1, i in keys(d)
+        r = nothing
+        try
+            r = @eval $f($i)
+        catch
+            println("# eval failed for $f($i)")
+        end
         if r in keys(d)
-            r1 = @eval $f(static($i))
+            r1 = nothing
+            try
+                r1 = @eval $f(static($i))
+            catch
+                nothing
+            end
             if r1 isa Static
                 @assert r1 == r
             else
                 println(f, "(::", d[i], ") = ", d[r], "()")
             end
         end
-    catch
-        println("# eval failed for $f($i)")
     end
 end
+fun1(f1, d)
 
-println("\n# Functions with two arguments")
-for f in f2, i in keys(d), j in keys(d)
-    try
-        r = @eval $f($i, $j)
+function fun2(f2, d)
+    println("\n# Functions with two arguments")
+    for f in f2, i in keys(d), j in keys(d)
+        r = nothing
+        try
+            r = @eval $f($i, $j)
+        catch
+            println("# eval failed for $f($i, $j)")
+        end
         if r in keys(d)
-            r1 = @eval $f(static($i), static($j))
+            r1 = nothing
+            try
+                r1 = @eval $f(static($i), static($j))
+            catch
+                nothing
+            end
             if r1 isa Static
                 @assert r1 == r
             else
                 println(f, "(::", d[i], ", ::", d[j], ") = ", d[r], "()")
             end
         end
-    catch
-        println("# eval failed for $f($i, $j)")
     end
 end
+fun2(f2, d)
 
-end
+end # module
