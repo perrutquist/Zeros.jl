392c57112ccec59732fda3a2ed4e731505eb9ad4
Per Rutquist <per.rutquist@gmail.com>
2020-06-23 12:47:56 +0200

    Work on removing ambiguities

--- src/Zeros.jl
+++ src/Zeros.jl
@@ -1,49 +1,90 @@
 module Zeros
 
-import Base: +, -, *, /, <, >, <=, >=, fma, muladd, mod, rem, modf,
+import Base: +, -, *, /, <, >, <=, >=, &, |, xor,
+     fma, muladd, mod, rem, modf, one, zero,
      ldexp, copysign, flipsign, sign, round, floor, ceil, trunc,
-     promote_rule, convert, show, significand, string
+     promote_rule, convert, show, significand, string,
      AbstractFloat, Integer, Complex
 
-export Zero, testzero, zero!
+export Zero, testzero, One, testone
 
  "A type that stores no data, and holds the value zero."
 struct Zero <: Integer
 end
 
+"A type that stores no data, and holds the value one."
+struct One <: Integer
+end
 
-promote_rule(::Type{Zero}, ::Type{T}) where {T<:Number} = T
-promote_rule(::Type{Zero}, ::Type{Zero}) = Float64
+const StaticBool = Union{Zero, One}
 
-convert(::Type{Zero}, ::Zero) = Zero()
+promote_rule(::Type{T}, ::Type{<:StaticBool}) where {T<:Number} = T
+promote_rule(::Type{T}, ::Type{<:StaticBool}) where {T<:Real} = T
+promote_rule(::Type{Complex{T}}, ::Type{<:StaticBool}) where {T<:Real} = Complex{T}
+promote_rule(::Type{Bool}, ::Type{<:StaticBool}) = Bool
+promote_rule(::Type{<:StaticBool}, ::Type{<:StaticBool}) = Bool
+
+convert(::Type{T}, x::T) where {T<:StaticBool} = x
+
 convert(::Type{T}, ::Zero) where {T<:Number} = zero(T)
-convert(::Type{Zero}, x::T) where {T<:Number} = Zero(x)
+convert(::Type{T}, ::One) where {T<:Number} = one(T)
+
+#convert(::Type{Zero}, x::T) where {T<:Number} = Zero(x)
 
 Zero(x::Number) = iszero(x) ? Zero() : throw(InexactError(:Zero, Zero, x))
+One(x::Number) = isone(x) ? One() : throw(InexactError(:One, One, x))
+convert(::Type{T}, x::StaticBool) where {T<:StaticBool} = throw(InexactError(:convert, T, x))
 
 AbstractFloat(::Zero) = 0.0
+AbstractFloat(::One) = 1.0
+
+zero(::StaticBool) = Zero()
+one(::StaticBool) = One()
 
 Complex(x::Real, ::Zero) = x
 
 # Loop over types in order to make methods specific enough to avoid ambiguities.
-for T in (Number, Real, Integer, Complex, Complex{Bool})
-    Base.:+(x::T,::Zero) = x
-    Base.:+(::Zero,x::T) = x
-    Base.:-(x::T,::Zero) = x
-    Base.:-(::Zero,x::T) = -x
-    Base.:*(::T,::Zero) = Zero()
-    Base.:*(::Zero,::T) = Zero()
+for T in (Number, Real, Rational, Integer, Complex, Complex{Bool})
+    Base.:+(x::T, ::Zero) = x
+    Base.:+(::Zero, x::T) = x
+    Base.:-(x::T, ::Zero) = x
+    Base.:-(::Zero, x::T) = -x
+    Base.:*(::T, ::Zero) = Zero()
+    Base.:*(::Zero, ::T) = Zero()
     Base.:/(::Zero, ::T) = Zero()
     Base.:/(::T, ::Zero) = throw(DivideError())
+    Base.:*(x::T, ::One) = x
+    Base.:*(::One, x::T) = x
+    Base.:/(x::T, ::One) = x
 end
 
-+(::Zero,::Zero) = Zero()
--(::Zero,::Zero) = Zero()
-*(::Zero,::Zero) = Zero()
-/(::Zero, ::Zero) = throw(DivideError())
+# These functions are intentionally not type-stable.
+"Convert to Zero() if equal to zero. (Use immediately before calling a function.)"
+testzero(x::Number) = iszero(x) ? Zero() : x
+"Convert to One() if equal to one. (Use immediately before calling a function.)"
+testone(x::Number) = isone(x) ? One() : x
 
-<(::Zero,::Zero) = false
-<=(::Zero,::Zero) = true
+# This functions give a strange "of_sametype" error. (See int.jl)
+# Hence we overload them all, even when result is not One() or Zero()
+for op in (:+, :-, :*, :&, :|, :xor)
+    for (T1,x) in ((:Zero, 0), (:One, 1))
+        for (T2,y) in ((:Zero, 0), (:One, 1))
+            val = @eval($op($x,$y))
+            val = testzero(val)
+            val = testone(val)
+            @eval $op(::$T1, ::$T2) = $val
+        end
+    end
+end
+
+-(::Zero) = Zero()
+-(::One) = -1
+
+/(::Zero, ::Zero) = throw(DivideError())
+/(::One, ::One) = One()
+
+<(::T,::T) where {T<:StaticBool} = false
+<=(::T,::T) where {T<:StaticBool} = true
 
 ldexp(::Zero, ::Integer) = Zero()
 copysign(::Zero,::Real) = Zero()
@@ -54,8 +95,9 @@ modf(::Zero) = (Zero(), Zero())
 # ==, !=, abs, isinf, isnan, isinteger, isreal, isimag,
 # signed, unsigned, float, big, complex, isodd, iseven
 
-for op in [:(-), :sign, :round, :floor, :ceil, :trunc, :significand]
-  @eval $op(::Zero) = Zero()
+for op in [:sign, :round, :floor, :ceil, :trunc, :significand]
+    @eval $op(::Zero) = Zero()
+    @eval $op(::One) = One()
 end
 
 # Avoid promotion of triplets
@@ -67,6 +109,8 @@ for op in [:fma :muladd]
         @eval $op(::Zero, x::$T, y::$T) = convert(promote_type(typeof(x),typeof(y)),y)
         @eval $op(x::$T, ::Zero, y::$T) = convert(promote_type(typeof(x),typeof(y)),y)
         @eval $op(x::$T, y::$T, ::Zero) = x*y
+        @eval $op(::One, x::$T, y::$T) = x+y
+        @eval $op(x::$T, ::One, y::$T) = x+y
     end
 end
 
@@ -74,23 +118,11 @@ for op in [:mod, :rem]
   @eval $op(::Zero, ::Real) = Zero()
 end
 
-# Display Zero() as `0̸` (unicode "zero with combining long solidus overlay")
-# so that it looks slightly different from `0`.
-show(io::IO, ::Zero) = print(io, "0̸")
-string(z::Zero) = Base.print_to_string(z)
+# Display Zero() as `𝟎` ("mathematical bold digit zero")
+# so that it looks slightly different from `0` (and same for One()).
+show(io::IO, ::Zero) = print(io, "𝟎") # U+1D7CE
+show(io::IO, ::One) = print(io, "𝟏") # U+1D7CF
 
-# Display Array{Zero} without printing the actual zeros.
-show(io::IO, x::Array{Zero,N}) where {N} = show(io, MIME"text/plain", x)
-function show(io::IO, ::MIME"text/plain", x::Array{Zero,N}) where {N}
-    join(io, map(dec, size(x)), "x")
-    print(io, " Array{Zero,", N, "}")
-end
-
-# This function is intentionally not type-stable.
-"Convert to Zero() if zero. (Use immediately before calling a function.)"
-testzero(x::Number) = x==zero(x) ? Zero() : x
-
-"Fill an array with zeros."
-zero!(a::Array{T}) where T = fill!(a, Zero())
+string(z::StaticBool) = Base.print_to_string(z)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,97 +1,125 @@
 using Zeros
 using Test
 
-# Real
-Z = Zero()
-@test Zero(0) === Z
-@test Z === Zero()
-@test Z == Z
-@test Z == 0
-@test sizeof(Z) == 0
-@test float(Z) === 0.0
-@test -Z === Z
-@test +Z === Z
-@test 2*Z === Z
-@test 2.0*Z === Z
-@test Z*3 === Z
-@test Z/2 === Z
-@test Z-Z === Z
-@test Z+Z === Z
-@test Z*Z === Z
-@test 1-Z === 1
-@test 1.0-Z === 1.0
-@test Z-1 == -1
-@test Z+1 == 1
-@test 2+Z == 2
-@test (Z < Z) == false
-@test (Z > Z) == false
-@test Z <= Z
-@test Z >= Z
-@test Z < 3
-@test Z > -2.0
-@test ldexp(Z, 3) === Z
-@test copysign(Z, 3) === Z
-@test copysign(Z, -1) === Z
-@test flipsign(Z, -1) === Z
-@test sign(Z) === Z
-@test round(Z) === Z
-@test floor(Z) === Z
-@test ceil(Z) === Z
-@test trunc(Z) === Z
-@test significand(Z) === Z
-@test !isodd(Z)
-@test iseven(Z)
-@test string(Z) == "0̸"
-@test fma(Z,1,Z) === Z
-@test muladd(Z,1,Z) === Z
-@test fma(Z,1,3) === 3
-@test muladd(Z,1,3) === 3
-@test fma(Z,Z,Z) === Z
-@test muladd(Z,Z,Z) === Z
-@test mod(Z, 3) === Z
-@test rem(Z, 3) === Z
-@test modf(Z) === (Z, Z)
-
-#Complex
-@test Z*im === Z
-@test im*Z === Z
-@test real(Z) === Z
-@test imag(Z) === Z
-@test Z+(2+3im) === 2+3im
-@test (2+3im)+Z === 2+3im
-@test Z-(2+3im) === -2-3im
-@test (2+3im)-Z === 2+3im
-@test Z*(2+3im) === Z
-@test (2.0+3.0im)*Z === Z
-@test Z/(2+3im) === Z
-
-@test Complex(Z,Z) == Z
-@test Complex(1,Z) == 1
-@test Complex(1.0,Z) == 1.0
-@test Complex(true,Z) == true
-
-# testzero()
-@test testzero(3) === 3
-@test testzero(3+3im) === 3+3im
-@test testzero(0) === Z
-@test testzero(0+0im) === Z
-
-# Array functions
-for cf in [(T)->T, (T)->Complex{T}]
-    for T in [UInt8, UInt16, UInt32, UInt64, UInt128, Int8, Int16, Int32, Int64, Int128, BigInt, Float16, Float32, Float64, BigFloat]
-        A = ones(cf(T),10)
-        @test zero!(A) === A
-        @test all(A .== zero(cf(T)))
+@testset "ambiguities" begin
+    ambiguities = detect_ambiguities(Base, Zeros)
+    for a in ambiguities
+        println(a[1], "\n", a[2], "\n")
     end
+    @test length(detect_ambiguities(Zeros)) == 0
+    @test length(ambiguities) <= 5
 end
 
-# Test error handling
-@test_throws InexactError Zero(1)
-@test_throws InexactError convert(Zero, 0.1)
-@test_throws DivideError 1.0/Z
-@test_throws DivideError (1.0+2.0im)/Z
-@test_throws DivideError Z/Z
+@testset "Real" begin
+    Z = Zero()
+    I = One()
+    @test iszero(Z)
+    @test isone(I)
+    @test !isone(Z)
+    @test !iszero(I)
+    @test Zero(0) === Z
+    @test One(1) === I
+    @test Z === Zero()
+    @test I === One()
+    @test Z == Z
+    @test I == I
+    @test Z == 0
+    @test I == 1
+    @test sizeof(Z) == 0
+    @test float(Z) === 0.0
+    @test float(I) === 1.0
+    @test -Z === Z
+    @test -I === -1
+    @test +Z === Z
+    @test +I === I
+    @test 2*Z === Z
+    @test Z*Z === Z
+    @test I*I === I
+    @test 2*I === 2
+    @test 2.0*Z === Z
+    @test 2.0*I === 2.0
+    @test Z*3 === Z
+    @test Z/2 === Z
+    @test Z-Z === Z
+    @test Z+Z === Z
+    @test Z*Z === Z
+    @test 1-Z === 1
+    @test 1.0-Z === 1.0
+    @test Z-1 == -1
+    @test Z+1 == 1
+    @test 2+Z == 2
+    @test (Z < Z) == false
+    @test (Z > Z) == false
+    @test Z <= Z
+    @test Z >= Z
+    @test Z < 3
+    @test Z > -2.0
+    @test ldexp(Z, 3) === Z
+    @test copysign(Z, 3) === Z
+    @test copysign(Z, -1) === Z
+    @test flipsign(Z, -1) === Z
+    @test sign(Z) === Z
+    @test round(Z) === Z
+    @test floor(Z) === Z
+    @test ceil(Z) === Z
+    @test trunc(Z) === Z
+    @test significand(Z) === Z
+    @test !isodd(Z)
+    @test iseven(Z)
+    @test string(Z) == "𝟎"
+    @test fma(Z,1,Z) === Z
+    @test fma(Z,1.0,Z) === Z
+    @test muladd(Z,1,Z) === Z
+    @test muladd(Z,1.0,Z) === Z
+    @test fma(Z,1,3) === 3
+    @test fma(Z,1.0,3) === 3.0
+    @test muladd(Z,1,3) === 3
+    @test muladd(Z,1.0,3) === 3.0
+    @test fma(Z,Z,Z) === Z
+    @test muladd(Z,Z,Z) === Z
+    @test mod(Z, 3) === Z
+    @test mod(Z, 3.0) === Z
+    @test rem(Z, 3) === Z
+    @test rem(Z, 3.0) === Z
+    @test modf(Z) === (Z, Z)
+end
+
+@testset "Complex" begin
+    @test Z*im === Z
+    @test im*Z === Z
+    @test real(Z) === Z
+    @test imag(Z) === Z
+    @test Z+(2+3im) === 2+3im
+    @test (2+3im)+Z === 2+3im
+    @test Z-(2+3im) === -2-3im
+    @test (2+3im)-Z === 2+3im
+    @test Z*(2+3im) === Z
+    @test (2.0+3.0im)*Z === Z
+    @test Z/(2+3im) === Z
+
+    @test Complex(Z,Z) == Z
+    @test Complex(1,Z) == 1
+    @test Complex(1.0,Z) == 1.0
+    @test Complex(true,Z) == true
+end
+
+@testset "testzero" begin
+    @test testzero(3) === 3
+    @test testzero(3+3im) === 3+3im
+    @test testzero(0) === Z
+    @test testzero(0+0im) === Z
+end
+
+@testset "error handling" begin
+    @test_throws InexactError Zero(1)
+    @test_throws InexactError convert(Zero, 0.1)
+    @test_throws DivideError 1.0/Z
+    @test_throws DivideError (1.0+2.0im)/Z
+    @test_throws DivideError Z/Z
+end
 
 # Test `MyComplex` example type
 include("mycomplex_example.jl")
-@test MyImaginary(2)*MyImaginary(3) === MyReal(-6)
+@testset "mycomplex_example.jl" begin
+    @test MyImaginary(2)*MyImaginary(3) === MyReal(-6)
+end
